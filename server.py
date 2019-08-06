443cd5ca29e42b5edc817686831023fe2a1d031d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-06 16:05:41 -0700

    added register user html template and route to handle new users

--- server.py
+++ server.py
@@ -32,6 +32,33 @@ def user_list():
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
+@app.route('/register')
+def register_form():
+    """Displays registration form"""
+
+    return render_template("register_form.html")
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    """Add new user to database"""
+
+    new_user_email = request.form.get("email")
+    new_user_password = request.form.get("password")
+    age = request.form.get("age")
+    zipcode = request.form.get("zipcode")
+
+    if User.query.filter(User.email==new_user_email).all() == []:
+        new_user = User(email=new_user_email, 
+                        password=new_user_password, 
+                        age=age,
+                        zipcode=zipcode
+                        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
