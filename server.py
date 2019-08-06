25bc03e17d14d1d1fd49c6cfe20669cef8b2ce0a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-06 15:22:47 -0700

    added route for user list

--- server.py
+++ server.py
@@ -25,6 +25,14 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
